77a1c48f8776c4f592b915f9c24057e526fb7f27
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2026-09-21 14:48:45 +0700

    Disable TLS in ReactBalancing.js
    Disable TLS and comment out the authEndpoint.

--- ReactBalancing.js
+++ ReactBalancing.js
@@ -4,7 +4,7 @@ const client = new Apinator({
   appId: '6d0f5089-b726-424d-a40f-de94928b3f72',
   appKey: 'app_a3bb62f84efd241f687d00cf7a851a38b152a355',
   secret: '56765c6b06d18969abe5d6a6ec70b79894910cf19d9b885f69433cd803f83d1d',
-  authEndpoint: '/api/realtime/auth'
+  useTLS: false //authEndpoint: '/api/realtime/auth'
 })
 
 client.connect()
