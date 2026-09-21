f2e47060e225cae7bdfb5a5792e88623e4dd6131
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2026-09-21 14:43:40 +0700

    Update Apinator configuration keys and endpoint

--- ReactBalancing.js
+++ ReactBalancing.js
@@ -2,9 +2,9 @@ import { Apinator } from "https://esm.sh/@apinator/client" //'@apinator/client'
 const client = new Apinator({
   cluster: 'us',
   appId: '6d0f5089-b726-424d-a40f-de94928b3f72',
-  key: 'app_a3bb62f84efd241f687d00cf7a851a38b152a355',
-  secret: '56765c6b06d18969abe5d6a6ec70b79894910cf19d9b885f69433cd803f83d1d'
-  //authEndpoint: '/api/realtime/auth'
+  appKey: 'app_a3bb62f84efd241f687d00cf7a851a38b152a355',
+  secret: '56765c6b06d18969abe5d6a6ec70b79894910cf19d9b885f69433cd803f83d1d',
+  authEndpoint: '/api/realtime/auth'
 })
 
 client.connect()
