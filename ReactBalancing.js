0da58fe43633ddad2cbdfe85c541acc4fc1f1822
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2026-09-21 13:25:57 +0700

    Add real-time alerts subscription

---
+++ ReactBalancing.js
@@ -0,0 +1,12 @@
+const client = new Apinator({
+  cluster: 'us',
+  authEndpoint: '/api/realtime/auth'
+})
+
+client.connect()
+
+// Public channel
+const alerts = client.subscribe('alerts')
+alerts.bind('new-alert',(data) => {
+  alert(data)
+})
